77818e614f9ef0237b96dc027c4dc654ecffe5f1
Melina-13 <melina.mouri@epitech.eu>
2025-04-01 14:14:58 +0200

    Add files via upload

---
+++ view/navbar_menu.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="public/css/navbar_menu.css">
+
+<nav>
+    <ul>
+        <li><a href="/home">A PROPOS</a></li>
+        <li><a href="/home#rubrique_projet">PROJETS</a></li>
+        <li><a href="/home#skills-section">SKILLS</a></li>
+        <li><a href="/home#rubrique-contact">CONTACT</a></li>
+    </ul>
+</nav>
